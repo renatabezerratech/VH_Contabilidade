af82f26d485a5d844bb786d7abda50104b122f54
Renata Bezerra <renatabezerratecnologia@gmail.com>
2021-07-29 12:33:57 -0300

    site

---
+++ scriptVH.js
@@ -0,0 +1,4 @@
+function clicou(){
+    var nome = window.prompt('Qual é o seu nome?');
+    window.alert(`Olá ${nome}, que bom te ver por aqui! Marcando uma consultoria comigo pelo whatsapp, você terá o direcionamento necessário para o que você precisa. Entenda como funciona a consultoria na segunda página e preencha o formulário na última página. Após, deixe seu recado pelo número é 21 99746-7242`);
+    }
